0be299e24461a53a467d20eb5f1afc09bb95bb12
sauravpandey538 <sauravpandey0325@icloud.com>
2024-07-06 17:42:32 +0545

    theme

--- src/app/printHelper/page.tsx
+++ src/app/printHelper/page.tsx
@@ -54,7 +54,7 @@ const PrintHelp: React.FC<ComponentNameProps> = ({  }) => {
     <HoverEffect items={projects} />
     <Link href={"/"}>
       <Button
-              className="bg-white dark:bg-black text-neutral-300 dark:text-white border-neutral-200 dark:border-slate-800 text-lg  "
+        className="bg-black text-white border-slate-800 text-lg  "
 
       >Go Back</Button>
       </Link>
